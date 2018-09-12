14aad7022d381fb1da56353698388dfe1a5e739f
tulasinandan <tulasinandan@gmail.com>
2018-09-12 14:38:47 -0400

    Add hdf5.mod for portability purposes?

--- src/NHDS.f90
+++ src/NHDS.f90
@@ -26,20 +26,6 @@
 !of the authors and should not be interpreted as representing official policies,
 !either expressed or implied, of the NHDS project.
 
-module input_params
-  implicit none
-  save
-  integer :: numspec,numiter,nmax,mmax,ampl_mode
-  integer :: vxsteps,vysteps,vzsteps,timesteps,num_periods,kzsteps
-  double precision :: vtherm(10),alpha(10),Omega(10),ell(10),vdrift(10),mass(10),charge(10),beta(10),density(10)
-  double precision :: vAc,det_D_threshold,Bessel_zero,ampl,theta,kzrange(2)
-  double precision :: Bessel_zero_deltaf,vxrange(2),vyrange(2),vzrange(2)
-  double complex :: initial_guess
-  double complex, parameter ::  uniti=(0.d0,1.d0)
-  double precision,parameter :: M_PI=3.141592654d0
-  logical :: output_warning,damping,periods,const_r
-end module input_params
-
 program NHDS
 use input_params
 implicit none
